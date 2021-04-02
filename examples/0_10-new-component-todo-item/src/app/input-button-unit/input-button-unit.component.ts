5448bef2ca0d6e39261168f521e1715cd6241030
Alisa Duncan <7586237+alisaduncan@users.noreply.github.com>
2021-04-02 18:00:56 -0500

    chore: update todo-item component instructions and answerkey

---
+++ examples/0_10-new-component-todo-item/src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <input #inputElementRef
+           [value]="title"
+           (keyup.enter)="changeTitle($event.target.value)">
+    <button (click)="changeTitle('Button Clicked!')">
+      Save
+    </button>
+  `,
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeTitle(newTitle: string): void {
+    this.title = newTitle;
+  }
+}
