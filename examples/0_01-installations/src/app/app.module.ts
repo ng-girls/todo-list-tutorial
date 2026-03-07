5f6831135632fc045a9e57a2b6994b9cf9b95bfd
Robert Willemelis <robert.dev@gmx.de>
2026-03-08 00:38:54 +0100

    fix: update chapter 01 to version 21

--- examples/0_01-installations/src/app/app.module.ts
+++ examples/0_01-installations/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { App } from './app';
 
 @NgModule({
   declarations: [
-    AppComponent
+    App
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [App]
 })
 export class AppModule { }
--- examples/0_01-installations/src/app/app.ts
+++ examples/0_01-installations/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: false,
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
 })
-export class AppComponent {
+export class App {
   title = 'todo-list';
 }
