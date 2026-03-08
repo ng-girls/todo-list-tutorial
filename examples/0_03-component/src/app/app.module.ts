92d69894adeb19ecb041d131c8aa6842f802c725
Robert Willemelis <robert.dev@gmx.de>
2026-03-08 01:49:33 +0100

    feat(chapter 3): migrate to version 21

--- examples/0_03-component/src/app/app.module.ts
+++ examples/0_03-component/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { App } from './app';
 
 @NgModule({
   declarations: [
-    AppComponent
+    App
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [App]
 })
 export class AppModule { }
--- examples/0_03-component/src/app/app.ts
+++ examples/0_03-component/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   template: `
     <h1>
       Welcome to {{ title }}!
     </h1>
   `,
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.scss']
 })
-export class AppComponent {
+export class App {
   title = 'todo-list';
 }
