5c90df6ce1cdb60614c19a02d057b13dcb5cea92
Robert Willemelis <robert.dev@gmx.de>
2026-03-08 01:59:33 +0100

    feat(chapter 4): update to v21

--- examples/0_04-a-new-component/src/app/app.ts
+++ examples/0_04-a-new-component/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   template: `
     <h1>
       Welcome to {{ title }}!
@@ -9,8 +10,8 @@ import { Component } from '@angular/core';
 
     <app-input-button-unit></app-input-button-unit>
   `,
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.scss']
 })
-export class AppComponent {
+export class App {
   title = 'todo-list';
 }
